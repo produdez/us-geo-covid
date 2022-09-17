a033aff31279b1507a9a470edfda967020e0b7d1
produdez <bigtimegangstar@gmail.com>
2022-09-17 12:10:41 +0700

    [feat/fix] empty component for details/graph (not implemented) (CAN RUN THIS COMMIT)

---
+++ src/app/detailed-page/graphs/graphs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphs',
+  templateUrl: './graphs.component.html',
+  styleUrls: ['./graphs.component.sass']
+})
+export class GraphsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
